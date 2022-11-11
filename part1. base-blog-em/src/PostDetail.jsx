da83c8245007ebdeaf0be36c0358f58af7299f8a
sungseokmin <jkl154527@gmail.com>
2022-11-11 12:26:06 +0900

    📝 quiz : comments list

--- part1. base-blog-em/src/PostDetail.jsx
+++ part1. base-blog-em/src/PostDetail.jsx
@@ -1,37 +1,43 @@
+import { useQuery } from 'react-query';
+
 async function fetchComments(postId) {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
-  );
+  const response = await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`);
   return response.json();
 }
 
 async function deletePost(postId) {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/postId/${postId}`,
-    { method: "DELETE" }
-  );
+  const response = await fetch(`https://jsonplaceholder.typicode.com/postId/${postId}`, {
+    method: 'DELETE',
+  });
   return response.json();
 }
 
 async function updatePost(postId) {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/postId/${postId}`,
-    { method: "PATCH", data: { title: "REACT QUERY FOREVER!!!!" } }
-  );
+  const response = await fetch(`https://jsonplaceholder.typicode.com/postId/${postId}`, {
+    method: 'PATCH',
+    data: { title: 'REACT QUERY FOREVER!!!!' },
+  });
   return response.json();
 }
 
 export function PostDetail({ post }) {
-  // replace with useQuery
-  const data = [];
+  const {
+    data: comments,
+    isLoading,
+    isError,
+  } = useQuery(['comments', post.id], () => fetchComments(post.id));
+
+  if (isLoading) return <h3>Loading ...</h3>;
+
+  if (isError) return <h3>isError ...</h3>;
 
   return (
     <>
-      <h3 style={{ color: "blue" }}>{post.title}</h3>
+      <h3 style={{ color: 'blue' }}>{post.title}</h3>
       <button>Delete</button> <button>Update title</button>
       <p>{post.body}</p>
       <h4>Comments</h4>
-      {data.map((comment) => (
+      {comments.map((comment) => (
         <li key={comment.id}>
           {comment.email}: {comment.body}
         </li>
