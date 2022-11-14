097a5517b3d7b7f0aab60efc3c6571de9b193de7
sungseokmin <jkl154527@gmail.com>
2022-11-14 20:58:57 +0900

    📝 quiz : useStaff

--- part3. lazy-days-spa/client/src/components/staff/hooks/useStaff.ts
+++ part3. lazy-days-spa/client/src/components/staff/hooks/useStaff.ts
@@ -1,4 +1,5 @@
 import { Dispatch, SetStateAction, useState } from 'react';
+import { useQuery } from 'react-query';
 
 import type { Staff } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
@@ -6,10 +7,11 @@ import { queryKeys } from '../../../react-query/constants';
 import { filterByTreatment } from '../utils';
 
 // for when we need a query function for useQuery
-// async function getStaff(): Promise<Staff[]> {
-//   const { data } = await axiosInstance.get('/staff');
-//   return data;
-// }
+async function getStaff(): Promise<Staff[]> {
+  const { data } = await axiosInstance.get('/staff');
+
+  return data;
+}
 
 interface UseStaff {
   staff: Staff[];
@@ -22,7 +24,8 @@ export function useStaff(): UseStaff {
   const [filter, setFilter] = useState('all');
 
   // TODO: get data from server via useQuery
-  const staff = [];
+
+  const { data: staff = [] } = useQuery([queryKeys.staff, filter], getStaff);
 
   return { staff, filter, setFilter };
 }
