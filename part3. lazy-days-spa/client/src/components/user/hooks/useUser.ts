603def9e8d71cbd671c67d5db35811f6015d6937
sungseokmin <jkl154527@gmail.com>
2022-11-21 09:06:40 +0900

    📝 setQueryData, removeQueries

--- part3. lazy-days-spa/client/src/components/user/hooks/useUser.ts
+++ part3. lazy-days-spa/client/src/components/user/hooks/useUser.ts
@@ -46,6 +46,7 @@ export function useUser(): UseUser {
   // meant to be called from useAuth
   function clearUser() {
     queryClient.setQueryData([queryKeys.user], null);
+    queryClient.removeQueries('user-appointments');
   }
 
   return { user, updateUser, clearUser };
